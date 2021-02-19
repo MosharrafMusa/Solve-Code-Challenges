69dd97d1f2651858a85a3cab7a56fa1eb146fd87
Mosharraf-Musa <mosharrafny@gmail.com>
2021-02-18 21:07:30 -0500

    solved-03

--- challenge02.py
+++ challenge02.py
@@ -1,5 +1,7 @@
 # The first century spans from the year 1 up to and including the year 100, The second - from the year 101 up to and including the year 200, etc.
 import math
+
+
 def century(year):
     # Finish this :)
     return math.ceil(year/100)

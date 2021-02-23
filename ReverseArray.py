c800ac8d31c069c68ba01fc779ae1f2c9ccf5022
Mosharraf-Musa <mosharrafny@gmail.com>
2021-02-22 20:50:48 -0500

    Reverse array

---
+++ ReverseArray.py
@@ -0,0 +1,8 @@
+# reverse array
+string = "tutorialspoint is a educational website"
+# splitting the string on space
+words = string.split()
+# reversing the words using reversed() function
+words = list(reversed(words))
+# joining the words and printing
+print(" ".join(words))

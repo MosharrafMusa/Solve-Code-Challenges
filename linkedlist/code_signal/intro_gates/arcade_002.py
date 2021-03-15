10824469783fe02abd247db48b8447473532b4e8
Mosharraf-Musa <mosharrafny@gmail.com>
2021-03-15 19:26:20 -0400

    solved arcade-002

---
+++ linkedlist/code_signal/intro_gates/arcade_002.py
@@ -0,0 +1,14 @@
+# Given an integer n, return the largest number that contains exactly n digits.
+
+# Example
+
+# For n = 2, the output should be
+# largestNumber(n) = 99.
+import math
+
+
+def largestNumber(n):
+    return math.pow(10, n)-1
+
+
+print(largestNumber(2))

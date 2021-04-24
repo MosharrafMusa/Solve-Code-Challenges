c463b188b822ab0fca819b488a12a9aa3647c37a
Mosharraf-Musa <mosharrafny@gmail.com>
2021-04-24 15:24:06 -0400

    array-reverse-in-place

---
+++ arrays/udemy_course/string_challenge/interview/reverse_array.py
@@ -0,0 +1,11 @@
+
+# use reverse() method in-place
+def reverse_array(l1):
+    l1.reverse()
+    return l1
+
+
+l1 = [10, 11, 12, 13, 14, 15]
+print(reverse_array(l1))
+
+# slice method is not in place

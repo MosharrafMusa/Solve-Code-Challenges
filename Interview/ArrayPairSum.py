9c594e7d8c033917042e250860da75b14dad68fa
Mosharraf Musa <mosharrafny@gmail.com>
2021-05-12 19:38:42 -0400

    solved

--- Interview/ArrayPairSum.py
+++ Interview/ArrayPairSum.py
@@ -1,2 +1,15 @@
 def array_pair_sum(arr1, s):
-    
+    seen= {}
+    result = []
+
+    for i in range(len(arr1)):
+        target = s - arr1[i]
+        if target in seen:
+            result.append((arr1(seen[temp]), arr1[i]))
+        else:
+            seen(arr1[i]) = i
+
+    return result
+
+arr1 = [1,3,2,2,5]
+print(array_pair_sum(arr1, 4))
